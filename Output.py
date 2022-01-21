756256f9b4c9543a87de5b70ab410ed1fde6476b
faraz-rahman-EE-21104024 <97098058+faraz-rahman-EE-21104024@users.noreply.github.com>
2022-01-21 13:54:56 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,30 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Faraz Rahman\Downloads\Introduction to Computing\Assignment 2\Input.py
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is an object oriented language
+The index of 'a' is: 10
+Pythonisacasesensitivelanguage
+Hey, Faraz Rahman here!
+My SID is 21104024
+I am from EE department and my CGPA is 10
+Result of the AND operation on a and b: 8
+Result of the OR operation on a and b: 58
+Result of the XOR operation on a and b: 50
+Result of left shifting a: 224
+Result of left shifting b: 40
+Result of right shifting a: 14
+Result of right shifting b: 0
+Enter first number: 3
+Enter second number: 6
+Enter third number: 5
+6 is the greatest number
+Enter a string: What is your name?
+Yes
+Enter first side: 4
+Enter second side: 7
+Enter third side: 10
+4, 7 and 10 are possible sides of a triangle
